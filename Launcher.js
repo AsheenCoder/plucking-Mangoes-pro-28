cf0c325cf5ea88bc4909e7d61c5866dd38e3d51e
AsheenCoder <78145227+AsheenCoder@users.noreply.github.com>
2021-05-30 11:20:49 +0530

    Add files via upload

---
+++ Launcher.js
@@ -0,0 +1,34 @@
+class Launcher {
+    constructor(bodyA,pointB){
+        var options ={
+            bodyA:bodyA,
+            pointB:pointB,
+            stiffness:0.1,
+            length:10
+        }
+    
+        this.launcherObject = Constraint.create(options)
+        World.add(world,this.launcherObject)
+
+        this.bodyA = bodyA
+        this.pointB = pointB
+    }
+
+    display(){
+
+        if(this.launcherObject.bodyA){
+           strokeWeight(3);
+            line(this.bodyA.position.x,this.bodyA.position.y,this.pointB.x,this.pointB.y);
+        }
+
+    }
+
+    fly(){
+        this.launcherObject.bodyA = null
+    }
+
+    attach(body){
+        this.launcherObject.bodyA = body;
+    }
+
+} 
